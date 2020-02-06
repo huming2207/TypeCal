edbcc8595babff5adb048e6cd51a6a5e6aa1fa09
Jackson Ming Hu <huming2207@gmail.com>
2020-02-06 16:26:46 +1100

    components: add type

---
+++ src/components/ComponentType.ts
@@ -0,0 +1,8 @@
+export enum ComponentType {
+    Event = 'VEVENT',
+    Alarm = 'VALARM',
+    Todo = 'VTODO',
+    FreeBusy = 'VFREEBUSY',
+    Journal = 'VJOURNAL',
+    Timezone = 'VTIMEZONE',
+}
