a2f3e9653a6af066532a8cfeb96e77d0ec6e1f7d
Jackson Ming Hu <huming2207@gmail.com>
2020-02-08 13:30:13 +1100

    StringHelper: add RegExp type for splitter

--- src/common/StringHelper.ts
+++ src/common/StringHelper.ts
@@ -1,5 +1,5 @@
 export class StringHelper {
-    public static splitNonEmpty(str: string, splitter: string, limit?: number): string[] {
+    public static splitNonEmpty(str: string, splitter: string | RegExp, limit?: number): string[] {
         const arr = str.split(splitter, limit);
         return arr.filter((elem: string) => {
             return elem.length > 0;
