b4d711544c93b0469b65c2f624197a0541650dec
Jackson Ming Hu <huming2207@gmail.com>
2020-02-09 17:23:03 +1100

    DurationParser: init

---
+++ src/parsers/ValueParsers/DurationParser.ts
@@ -0,0 +1,37 @@
+import { CalDuration } from '../../properties/CalDuration';
+import { ValueParser } from './ValueParser';
+import { Duration, DateTime } from 'luxon';
+
+export class DurationParser implements ValueParser<CalDuration> {
+    public parse = (rawStr: string): CalDuration => {
+        const calDuration = new CalDuration();
+        if (rawStr.includes('RELATED=')) {
+            // Scenario 1: Get RELATED string first if there's a RELATED value
+            const related = rawStr.match(/(?<=RELATED\=)(.*)(?=\:)/gm);
+            if (related === null) throw SyntaxError(`Cannot parse CalDuration property: ${rawStr}, invalid RELATED parameter.`);
+            calDuration.related = related[0];
+            const durationStr = rawStr.split(':', 2)[1];
+            const duration = Duration.fromISO(durationStr)
+                .shiftTo('seconds')
+                .toObject().seconds;
+            if (duration === undefined) throw SyntaxError(`Cannot parse CalDuration property: ${rawStr}, invalid duration.`);
+            calDuration.durationSec = duration;
+        } else if (rawStr.includes('VALUE=DATE-TIME')) {
+            // Scenario 2: DateTime ISO8601 style
+            const dateTime = DateTime.fromISO(rawStr.split(':', 2)[1], { zone: 'UTC', setZone: true });
+            calDuration.atDate = dateTime.toJSDate();
+        } else {
+            // Scenario 3: Assume it's a duration-only scenario
+            const durationStr = rawStr.split(':', 2)[1];
+            const duration = Duration.fromISO(durationStr)
+                .shiftTo('seconds')
+                .toObject().seconds;
+            if (duration === undefined) throw SyntaxError(`Cannot parse CalDuration property: ${rawStr}, invalid duration.`);
+            calDuration.durationSec = duration;
+        }
+
+        return calDuration;
+    };
+
+    public propNames = ['TRIGGER', 'DURATION'];
+}
