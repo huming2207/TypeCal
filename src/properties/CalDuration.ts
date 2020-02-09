26f7daa4140795045be701b05193e7e517dd35c1
Jackson Ming Hu <huming2207@gmail.com>
2020-02-09 17:22:20 +1100

    CalDuration: init

---
+++ src/properties/CalDuration.ts
@@ -0,0 +1,37 @@
+import { serializeAs } from 'cerialize';
+import { DateSerializer } from '../common/DateSerializer';
+
+export class CalDuration {
+    @serializeAs('durationSec')
+    private _durationSec = NaN;
+
+    public get durationSec(): number {
+        return this._durationSec;
+    }
+
+    public set durationSec(value) {
+        this._durationSec = value;
+    }
+
+    @serializeAs(DateSerializer, 'atDate')
+    private _atDate = new Date('invalid');
+
+    public get atDate(): Date {
+        return this._atDate;
+    }
+
+    public set atDate(value) {
+        this._atDate = value;
+    }
+
+    @serializeAs('relatedTo')
+    private _related = '';
+
+    public get related(): string {
+        return this._related;
+    }
+
+    public set related(value: string) {
+        this._related = value;
+    }
+}
