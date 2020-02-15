f9185a456e72b43a849fb94e52804d34d49fb986
Jackson Ming Hu <huming2207@gmail.com>
2020-02-16 10:08:24 +1100

    StringHelp: giving up my own omitEmpty() function for now...

--- src/common/StringHelper.ts
+++ src/common/StringHelper.ts
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable @typescript-eslint/explicit-function-return-type */
 export class StringHelper {
     public static splitNonEmpty(str: string, splitter: string | RegExp, limit?: number): string[] {
         const arr = str.split(splitter, limit);
@@ -7,17 +5,4 @@ export class StringHelper {
             return elem.length > 0;
         });
     }
-
-    public static removeEmptyJsonObj = (obj: any) => {
-        Object.entries(obj).forEach(([key, val]) => {
-            if (val && typeof val === 'object') StringHelper.removeEmptyJsonObj(val);
-            else if (val == null) {
-                delete obj[key];
-            } else if (typeof val === 'string' && val === '') {
-                delete obj[key];
-            } else if (typeof val === 'number' && val === NaN) {
-                delete obj[key];
-            }
-        });
-    };
 }
