89187c5cf5e6c20bdf13926a256a0bce7e470bd1
Jackson Ming Hu <huming2207@gmail.com>
2020-02-19 23:09:26 +1100

    Add rrule.js dependencies

--- src/parsers/ComponentParsers/TimezoneParser.ts
+++ src/parsers/ComponentParsers/TimezoneParser.ts
@@ -4,6 +4,12 @@ import { TimezoneComponent } from '../../components/TimezoneComponent';
 export class TimezoneParser extends ComponentParser<TimezoneComponent> {
     public parseComponent = (rawStr: string): TimezoneComponent => {
         const tzComponent = new TimezoneComponent();
+        const result = rawStr.match(/(?<=TZID\:)(.*)/gm);
+        if (result === null || result.length > 1) {
+            throw new SyntaxError('Cannot find TZID in this VTIMEZONE section');
+        }
+
+        tzComponent.tzid = result[0];
         return tzComponent;
     };
 }
